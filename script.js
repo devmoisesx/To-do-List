893b727a810343915ced890009195182c300409b
Moisés Xavier <secondmox91@gmail.com>
2024-07-19 18:49:27 -0300

    refactor: Corrige bug que impedia as notas aparecerem

--- script.js
+++ script.js
@@ -1,220 +1,20 @@
-// const listElement = document.querySelector(".list");
-// const inputText = document.querySelector("#text-item");
-// const addBtn = document.querySelector(".btn-add");
-// const accordionBtn = document.querySelector(".btn-open-section");
-// const boxCheckedItems = document.querySelector(".checked-items");
-
-// let arr = {};
-// let checkedItemsObj = {};
-
-// // function renderTodo() {
-// //   listElement.innerText = "";
-
-// //   for (let [index, text] of Object.entries(arr)) {
-// //     const note = document.createElement("li");
-// //     note.classList.add("note");
-
-// //     const noteUtilities = document.createElement("div");
-// //     noteUtilities.classList.add("utilities-note");
-
-// //     const utilitieCheck = document.createElement("input");
-// //     utilitieCheck.setAttribute("type", "checkbox");
-// //     utilitieCheck.setAttribute("name", "check");
-// //     utilitieCheck.setAttribute("id", "check");
-// //     utilitieCheck.classList.add("check");
-// //     utilitieCheck.addEventListener("click", () =>
-// //       checkItems(index, utilitieCheck)
-// //     );
-// //     // utilitieCheck.addEventListener("click", () => checkItem(index));
-// //     noteUtilities.appendChild(utilitieCheck);
-
-// //     // const utilitieDelete = document.createElement("span");
-// //     // utilitieDelete.innerText = "x";
-// //     // utilitieDelete.classList.add("delete");
-// //     // utilitieDelete.addEventListener("click", () => deleteItem(index));
-// //     // noteUtilities.appendChild(utilitieDelete);
-
-// //     note.appendChild(noteUtilities);
-
-// //     const noteTitle = document.createElement("div");
-// //     noteTitle.classList.add("title-note");
-// //     note.appendChild(noteTitle);
-// //     const title = document.createElement("h2");
-// //     title.innerText = text;
-// //     noteTitle.appendChild(title);
-
-// //     listElement.appendChild(note);
-// //   }
-
-// //   for (let [index, text] of Object.entries(checkedItemsObj)) {
-// //     const note = document.createElement("li");
-// //     note.classList.add("note");
-
-// //     const noteUtilities = document.createElement("div");
-// //     noteUtilities.classList.add("utilities-note");
-
-// //     const utilitieCheck = document.createElement("input");
-// //     utilitieCheck.setAttribute("type", "checkbox");
-// //     utilitieCheck.setAttribute("name", "check");
-// //     utilitieCheck.setAttribute("id", "check");
-// //     utilitieCheck.classList.add("check");
-// //     utilitieCheck.addEventListener("click", () =>
-// //       checkItems(index, utilitieCheck)
-// //     );
-// //     // utilitieCheck.addEventListener("click", () => checkItem(index));
-// //     noteUtilities.appendChild(utilitieCheck);
-
-// //     // const utilitieDelete = document.createElement("span");
-// //     // utilitieDelete.innerText = "x";
-// //     // utilitieDelete.classList.add("delete");
-// //     // utilitieDelete.addEventListener("click", () => deleteItem(index));
-// //     // noteUtilities.appendChild(utilitieDelete);
-
-// //     note.appendChild(noteUtilities);
-
-// //     const noteTitle = document.createElement("div");
-// //     noteTitle.classList.add("title-note");
-// //     note.appendChild(noteTitle);
-// //     const title = document.createElement("h2");
-// //     title.innerText = text;
-// //     noteTitle.appendChild(title);
-
-// //     boxCheckedItems.appendChild(note);
-// //   }
-// // }
-
-// function createNote(index, text, isChecked, parentElement) {
-//   const note = document.createElement("li");
-//   note.classList.add("note");
-
-//   const noteUtilities = document.createElement("div");
-//   noteUtilities.classList.add("utilities-note");
-
-//   const utilitieCheck = document.createElement("input");
-//   utilitieCheck.setAttribute("type", "checkbox");
-//   utilitieCheck.setAttribute("name", "check");
-//   utilitieCheck.setAttribute("id", "check");
-//   utilitieCheck.classList.add("check");
-//   utilitieCheck.checked = isChecked;
-//   utilitieCheck.addEventListener("click", () =>
-//     checkItems(index, utilitieCheck)
-//   );
-//   noteUtilities.appendChild(utilitieCheck);
-
-//   note.appendChild(noteUtilities);
-
-//   const noteTitle = document.createElement("div");
-//   noteTitle.classList.add("title-note");
-//   note.appendChild(noteTitle);
-//   const title = document.createElement("h2");
-//   title.innerText = text;
-//   noteTitle.appendChild(title);
-
-//   parentElement.appendChild(note);
-// }
-
-// function renderTodo() {
-//   listElement.innerText = "";
-//   boxCheckedItems.innerText = "";
-
-//   for (let [index, text] of Object.entries(arr)) {
-//     createNote(index, text, false, listElement);
-//   }
-
-//   for (let [index, text] of Object.entries(checkedItemsObj)) {
-//     createNote(index, text, true, boxCheckedItems);
-//   }
-// }
-
-// addBtn.addEventListener("click", addItens);
-
-// function addItens() {
-//   if (inputText.value) {
-//     arr[Object.keys(arr).length + 1] = inputText.value;
-//   }
-//   renderTodo();
-//   return saveList();
-// }
-
-// inputText.addEventListener("keyup", function (e) {
-//   var key = e.which || e.keyCode;
-//   if (key == 13) {
-//     // codigo da tecla enter
-//     return addItens();
-//   }
-// });
-
-// function checkItems(index, utilitieCheck) {
-//   utilitieCheck.checked = !!utilitieCheck.checked;
-//   if (arr[index] !== undefined && arr[index] !== checkedItemsObj[index]) {
-//     checkedItemsObj[Object.keys(checkedItemsObj).length + 1] = arr[index];
-//     delete arr[index];
-//   } else if (checkedItemsObj[index] !== undefined) {
-//     delete checkedItemsObj[index];
-//   }
-//   renderTodo();
-//   console.log(checkedItemsObj);
-// }
-
-// function deleteItem(target) {
-//   delete arr[target];
-//   localStorage.removeItem(target);
-//   renderTodo();
-//   return saveList();
-// }
-
-// function saveList() {
-//   for (let [index, text] of Object.entries(arr)) {
-//     localStorage.setItem(index, text);
-//   }
-// }
-
-// function renderSaveList() {
-//   if (localStorage.length > 0) {
-//     const chaves = Object.keys(localStorage);
-//     const chavesOrdenadas = Array.from(chaves).sort((a, b) =>
-//       a.localeCompare(b)
-//     );
-//     for (let i = 0; i < chavesOrdenadas.length; i++) {
-//       const chave = chavesOrdenadas[i];
-//       arr[chave] = localStorage[chave];
-//     }
-//   }
-// }
-
-// accordionBtn.addEventListener("click", () => {
-//   accordionBtn.classList.toggle("active");
-// });
-
-// renderSaveList();
-
-// renderTodo();
-
-// Seleciona os elementos do DOM
-
-// ======================================================
-// ======================================================
-// ======================================================
-
 const listElement = document.querySelector(".list");
 const inputText = document.querySelector("#text-item");
 const addBtn = document.querySelector(".btn-add");
 const accordionBtn = document.querySelector(".btn-open-section");
-const boxCheckedItems = document.querySelector(".checked-items");
+const boxCheckedItems = document.querySelector(".checked-items > .list");
 
-function Task(title, description, check) {
+function Task(title, check) {
   this.title = title;
-  this.description = description;
   this.isChecked = check;
 }
 
 let array = [];
 
 // Função para criar uma nota
-function createNote(index, title, description, isChecked) {
+function createNote(index, title, isChecked) {
   const note = document.createElement("li");
   note.classList.add("note");
-  
 
   const noteUtilities = document.createElement("div");
   noteUtilities.classList.add("utilities-note");
@@ -250,8 +50,8 @@ function renderTodo() {
   listElement.innerText = "";
   boxCheckedItems.innerText = "";
 
-  for (let [index, { title, description, isChecked }] of array.entries()) {
-    createNote(index, title, description, isChecked);
+  for (let [index, { title, isChecked }] of array.entries()) {
+    createNote(index, title, isChecked);
   }
 }
 
@@ -259,7 +59,7 @@ addBtn.addEventListener("click", addItens);
 
 function addItens() {
   if (inputText.value) {
-    array.push(new Task(inputText.value, null, false));
+    array.push(new Task(inputText.value, false));
   }
   renderTodo();
   saveList();
@@ -310,8 +110,8 @@ function saveList() {
 
 function renderSaveList() {
   if (localStorage.length > 0) {
-    const tasks = JSON.parse(`[${localStorage.getItem("tasks")}]`)
-    array = []
+    const tasks = JSON.parse(`[${localStorage.getItem("tasks")}]`);
+    array = [];
     for (let [index, object] of tasks.entries()) {
       array.push(object);
     }
